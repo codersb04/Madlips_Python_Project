db16bd1bb18c04fa5ca3b2dc6ce75f462b489c8e
codersb04 <subithbabu197@gmail.com>
2023-02-15 12:34:15 +0000

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,18 @@
+# name ="Subith Babu"
+# print("Subscribe to " + name)
+# print("Subscribe to {}".format(name))
+# print(f"Subscribe to {name}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb is: ")
+verb2 = input ("Verb is:")
+famous_person= input("Famous Person :")
+
+
+
+
+madlips= f"Computer programming is so {adj}. It makes me so exicted all the time because \ I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print (madlips)
+
+
