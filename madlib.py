744a49c9811c9ecb2a17e3f13bda0468d910a322
codersb04 <subithbabu197@gmail.com>
2023-02-15 12:46:47 +0000

    Add the Madlip line

--- madlib.py
+++ madlib.py
@@ -3,6 +3,7 @@
 # print("Subscribe to {}".format(name))
 # print(f"Subscribe to {name}")
 
+print ("Computer programming is so {adj}. It makes me so exicted all the time because \ I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}")
 adj = input("Adjective: ")
 verb1 = input("Verb is: ")
 verb2 = input ("Verb is:")
