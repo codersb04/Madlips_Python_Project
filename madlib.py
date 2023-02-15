38c7d9b0c02581959bb4b74c6a78274fde021612
codersb04 <subithbabu197@gmail.com>
2023-02-15 13:14:09 +0000

    Final Code

--- madlib.py
+++ madlib.py
@@ -1,4 +1,8 @@
-# name ="Subith Babu"
+#First Python program. Basic program for the game Madlips.
+# Used the simple string concatenation for this.
+
+# Some sample of String Concatenations
+# name ="Richard Denise"
 # print("Subscribe to " + name)
 # print("Subscribe to {}".format(name))
 # print(f"Subscribe to {name}")
@@ -8,9 +12,6 @@ adj = input("Adjective: ")
 verb1 = input("Verb is: ")
 verb2 = input ("Verb is:")
 famous_person= input("Famous Person :")
-
-
-
 
 madlips= f"Computer programming is so {adj}. It makes me so exicted all the time because \ I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
 
